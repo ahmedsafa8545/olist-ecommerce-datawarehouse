2e0c1f1b678441917079f3ededd82c68b18affbf
ahmedsafa8545 <ahmedsafa8545@gmail.com>
2026-07-22 11:30:15 +0300

    Create bronze_quality_checks.sql

---
+++ scripts/bronze/bronze_quality_checks.sql
@@ -0,0 +1,26 @@
+-- 1. Customers
+SELECT TOP 100 * FROM bronze.olist_customers;
+
+-- 2. Geolocation
+SELECT TOP 100 * FROM bronze.olist_geolocation;
+
+-- 3. Order Items
+SELECT TOP 100 * FROM bronze.olist_order_items;
+
+-- 4. Order Payments
+SELECT TOP 100 * FROM bronze.olist_order_payments;
+
+-- 5. Order Reviews (check Portuguese accents here specifically)
+SELECT TOP 100 * FROM bronze.olist_order_reviews;
+
+-- 6. Orders
+SELECT TOP 100 * FROM bronze.olist_orders;
+
+-- 7. Products
+SELECT TOP 100 * FROM bronze.olist_products;
+
+-- 8. Sellers
+SELECT TOP 100 * FROM bronze.olist_sellers;
+
+-- 9. Product Category Name Translation
+SELECT TOP 100 * FROM bronze.product_category_name_translation;
